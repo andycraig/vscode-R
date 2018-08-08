b32a5ac14350519906ce4734319f1a4a98137fd8
Andrew Craig <apc53c@gmail.com>
2018-08-08 08:18:40 +0100

    Renamed getNextCharAndPos and processRestOfLine for clarity. Changed == to ===.

--- src/extendSelection.ts
+++ src/extendSelection.ts
@@ -50,14 +50,14 @@ class LineCache {
 
 function doBracketsMatch(a: string, b: string): boolean {
     const matches = { "(":")", "[":"]", "{":"}", ")":"(", "]":"[", "}":"{" };
-    return matches[a] == b;
+    return matches[a] === b;
 }
 
 function isBracket(c: string, forward: boolean) {
     if (forward) {
-        return ((c == "(") || (c == "[") || (c == "{"));
+        return ((c === "(") || (c === "[") || (c === "{"));
     } else {
-        return ((c == ")") || (c == "]") || (c == "}"));
+        return ((c === ")") || (c === "]") || (c === "}"));
     }
 }
 
@@ -91,7 +91,8 @@ export function doesLineEndInOperator(text: string) {
 }
 
 /**
- * From a given position, return the 'next' character and some associated information.
+ * From a given position, return the 'next' character, its position, and whether it is at the end of an 
+ * 'extended' line.
  * The next character may be on a different line if at the start/end of a line, or it may be the
  * same character if at the start/end of a document.
  * Considers the start and end of each line to be special distinct characters.
@@ -101,7 +102,7 @@ export function doesLineEndInOperator(text: string) {
  * @param getDoesLineEndInOperator A function that returns whether the given line ends in an operator.
  * @param lineCount The number of lines in the document.
  */
-export function getNextCharAndPos(p: PositionNeg, lookingForward: boolean, getLine: (number) => string, getDoesLineEndInOperator: (number) => boolean, lineCount) {
+export function getNextChar(p: PositionNeg, lookingForward: boolean, getLine: (number) => string, getDoesLineEndInOperator: (number) => boolean, lineCount) {
     const s = getLine(p.line);
     let nextPos: PositionNeg = null;
     let endOfCodeLine = false;
@@ -115,8 +116,8 @@ export function getNextCharAndPos(p: PositionNeg, lookingForward: boolean, getLi
             nextPos = new PositionNeg(p.line, p.character);
         }
         const nextLine: string = getLine(nextPos.line);
-        if (nextPos.character == nextLine.length) {
-            if ((nextPos.line == (lineCount - 1)) || !getDoesLineEndInOperator(nextPos.line)) {
+        if (nextPos.character === nextLine.length) {
+            if ((nextPos.line === (lineCount - 1)) || !getDoesLineEndInOperator(nextPos.line)) {
                 endOfCodeLine = true;
             }
         }
@@ -129,7 +130,7 @@ export function getNextCharAndPos(p: PositionNeg, lookingForward: boolean, getLi
             // At start of document. Return same charater.
             nextPos = new PositionNeg(p.line, p.character);
         }
-        if (nextPos.character == -1) {
+        if (nextPos.character === -1) {
             if ((nextPos.line <= 0) || !getDoesLineEndInOperator(nextPos.line - 1)) {
                 endOfCodeLine = true;
             }
@@ -137,9 +138,9 @@ export function getNextCharAndPos(p: PositionNeg, lookingForward: boolean, getLi
     }
     // Represent the start and end of the line with special characters.
     let nextChar = '';
-    if (nextPos.character == s.length) {
+    if (nextPos.character === s.length) {
         nextChar = "EOL";
-    } else if (nextPos.character == -1) {
+    } else if (nextPos.character === -1) {
         nextChar = "START_OF_LINE";
     } else {
         nextChar = getLine(nextPos.line)[nextPos.character];
@@ -165,18 +166,18 @@ export function findMatchingBracket(b: string, pos: PositionNeg, getLine: (numbe
     var endOfCodeLine = false;
     let possibleMatch = '';
     while (!doBracketsMatch(possibleMatch, b) && !flagAbort) { 
-        var { nextChar, nextPos, endOfCodeLine } = getNextCharAndPos(nextPos, lookingForward, getLine, getDoesLineEndInOperator, lineCount);
+        var { nextChar, nextPos, endOfCodeLine } = getNextChar(nextPos, lookingForward, getLine, getDoesLineEndInOperator, lineCount);
         if (isBracket(nextChar, lookingForward)) {
             unmatchedBrackets.push(nextChar);
         } else if (isBracket(nextChar, !lookingForward)) {
-           if (unmatchedBrackets.length == 0) {
+           if (unmatchedBrackets.length === 0) {
                 possibleMatch = nextChar;
             } else if (!doBracketsMatch(nextChar, unmatchedBrackets.pop())) {
                 flagAbort = true;
             }
         }
-        let atStartOfFile = !lookingForward && (nextPos.line == 0) && (endOfCodeLine);
-        let atEOF = lookingForward && (nextPos.line == (lineCount - 1)) && (endOfCodeLine);
+        let atStartOfFile = !lookingForward && (nextPos.line === 0) && (endOfCodeLine);
+        let atEOF = lookingForward && (nextPos.line === (lineCount - 1)) && (endOfCodeLine);
         if (atStartOfFile || atEOF) {
             // Have hit the start or end of the file without finding the matching bracket.
             flagAbort = true;
@@ -191,13 +192,13 @@ export function findMatchingBracket(b: string, pos: PositionNeg, getLine: (numbe
  * @param pos The position at which to start looking. The first position AFTER this will be the first one checked.
  * @param getLine A function that returns the string at the given line.
  * @param getDoesLineEndInOperator A function that returns whether the given line ends in an operator.
- * @param lookingForward true if looking for a bracket toward the end of the document, false for looking toward the start.
+ * @param lookingForward true if looking toward the end of the document, false for looking toward the start.
  * @param lineCount The number of lines in the document.
  */
-export function processRestOfExtendedLine(pos: PositionNeg, getLine: (number) => string, getDoesLineEndInOperator: (number) => boolean, lookingForward: boolean, lineCount: number) {
-    var { nextChar, nextPos, endOfCodeLine } = getNextCharAndPos(pos, lookingForward, getLine, getDoesLineEndInOperator, lineCount);
+export function findBracketOrLineTermination(pos: PositionNeg, getLine: (number) => string, getDoesLineEndInOperator: (number) => boolean, lookingForward: boolean, lineCount: number) {
+    var { nextChar, nextPos, endOfCodeLine } = getNextChar(pos, lookingForward, getLine, getDoesLineEndInOperator, lineCount);
     while (!endOfCodeLine && !(isBracket(nextChar, true) || isBracket(nextChar, false))) {
-        var { nextChar, nextPos, endOfCodeLine } = getNextCharAndPos(nextPos, lookingForward, getLine, getDoesLineEndInOperator, lineCount);
+        var { nextChar, nextPos, endOfCodeLine } = getNextChar(nextPos, lookingForward, getLine, getDoesLineEndInOperator, lineCount);
     }
     return ({ nextChar: nextChar, nextPos: nextPos, endOfCodeLine: endOfCodeLine });
 }
@@ -221,11 +222,11 @@ export function extendSelection(line: number, getLine: (number) => string, lineC
     let flagFinish = { 0: false, 1: false }; // 1 represents looking forward, 0 represents looking back.
     var flagAbort = false;
     // Check characters on current line. If a bracket, extend to the corresponding
-    // matching bracket. If the end of an 'extended line' is reached, we are finished
+    // matching bracket. If the termination of an 'extended line' is reached, we are finished
     // extending in that direction. Continue until there are no more unmatched brackets, 
     // and the we have reached the ends of the 'extended lines' both forwards and backwards.
     while (!flagAbort && (!flagFinish[0] || !flagFinish[1])) {
-        let { nextChar, nextPos, endOfCodeLine } = processRestOfExtendedLine(poss[lookingForward ? 1 : 0], getLineFromCache, getEndsInOperatorFromCache, lookingForward, lineCount);
+        let { nextChar, nextPos, endOfCodeLine } = findBracketOrLineTermination(poss[lookingForward ? 1 : 0], getLineFromCache, getEndsInOperatorFromCache, lookingForward, lineCount);
         if (isBracket(nextChar, true) || isBracket(nextChar, false)) {
             // Check which direction in which we need to look for the matching bracket.
             lookingForward = isBracket(nextChar, true);
