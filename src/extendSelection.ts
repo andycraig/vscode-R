0f7bb9af98c5284a95bbd8656d5b410a74346c15
Andrew Craig <apc53c@gmail.com>
2018-08-13 18:24:02 +0100

    Added explanation of algorithm and example.

--- src/extendSelection.ts
+++ src/extendSelection.ts
@@ -73,14 +73,13 @@ function doesLineEndInOperator(text: string) {
 }
 
 /**
- * From a given position, return the 'next' character, its position, whether it is at the termination of an 
- * 'extended' line, and whether it is the end of the file.
- * The next character may be on a different line if at the start/end of a line, or it may be the
- * same character if at the start/end of a document.
+ * From a given position, return the 'next' character, its position in the document,
+ * whether it is start/end of a code line broken over multiple text lines, and whether it is the 
+ * start/end of the file.
  * Considers the start and end of each line to be special distinct characters.
  * @param p The starting position.
- * @param lookingForward true if looking for a bracket toward the end of the document, false for looking toward the start.
- * @param getLine A function that returns the string at the given line.
+ * @param lookingForward true if the 'next' character is toward the end of the document, false if toward the start of the document.
+ * @param getLine A function that returns the string at the given line of the document.
  * @param getDoesLineEndInOperator A function that returns whether the given line ends in an operator.
  * @param lineCount The number of lines in the document.
  */
@@ -111,7 +110,7 @@ function getNextChar(p: PositionNeg, lookingForward: boolean, getLine: (number) 
         } else if (p.line > 0) { 
             nextPos = new PositionNeg(p.line - 1, getLine(p.line - 1).length - 1);
         } else {
-            // At start of document. Return same charater.
+            // At start of document. Return same character.
             isEndOfFile = true;
             nextPos = new PositionNeg(p.line, p.character);
         }
@@ -121,17 +120,43 @@ function getNextChar(p: PositionNeg, lookingForward: boolean, getLine: (number) 
             }
         }
     }
-    // Represent the start and end of the line with special characters.
     let nextChar = getLine(nextPos.line)[nextPos.character];
     return ({ nextChar: nextChar, nextPos: nextPos, isEndOfCodeLine: isEndOfCodeLine, isEndOfFile: isEndOfFile });
 }
 
 /**
- * Given a line number, determines the first and last lines required to include all the matching brackets 
- * and all the 'extended lines' (complete lines of code, possibly split into multiple lines
- * each ending in an operator) from that line.
+ * Given a line number, gets the text of that line and determines the first and last lines of the 
+ * file required to make a complete line of code, by matching brackets and extending over
+ * broken lines (single lines of code split into multipe text lines, joined by operators).
+ * 
+ * The algorithm:
+ * From the start of the given line, proceed forward looking for the end of the code line. 
+ * If a bracket is encountered, look for the match of that bracket (possibly changing direction to do so),
+ * from the furthest point reached in that direction. 
+ * Once the bracket is found, proceed in the same direction looking for the completion of the code line.
+ * Once the end of the code line has been matched, proceed in the other direction. Repeat until 
+ * all encountered brackets are matched, and the completions of the code lines have been reached in 
+ * both directions.
+ * 
+ * Example:
+ * Let's say we have the following R code file:
+ * 
+ *     library(magrittr) # For %>%    Line 1
+ *     list(x = 1,       #            Line 2
+ *          y = 2) %>%   #            Line 3
+ *         print()       #            Line 4
+ * 
+ * Let's say the cursor is on Line 3. We proceed forward until we hit the ')'. We look for the match, which 
+ * means looking backwards from the start of Line 2. We find the match, '(', on Line 2. We continue along 
+ * Line 2 until we reach the start of the line. The previous line, Line 1, does not end in an operator,
+ * so we have reached the completion of the code line. Now, we proceed forward again from the furthest point reached:
+ * the ')' on Line 3. We encounter the end of the TEXT line, but it ends in an operator '%>%', so it is not
+ * the end of the code line. Therefore, we continue onto Line 4. We encounter a '(' on Line 4, and continue 
+ * forward to find its match, which is the next character. Then we're at the end of Line 4, which doesn't
+ * end in an operator. Now we've found the completions in both directions, so we're finished. The furthest lines
+ * reached were Line 2 and Line 4, so those are the values returned.
  * @param line The line of the document at which to start.
- * @param getLine A function that returns the string at the given line.
+ * @param getLine A function that returns the string at the given line of the document.
  * @param lineCount The number of lines in the document.
  */
 export function extendSelection(line: number, getLine: (number) => string, lineCount: number) {
@@ -144,13 +169,7 @@ export function extendSelection(line: number, getLine: (number) => string, lineC
     let poss = { 0: new PositionNeg(line, 0), 1: new PositionNeg(line, -1) };
     let flagsFinish = { 0: false, 1: false }; // 1 represents looking forward, 0 represents looking back.
     let flagAbort = false;
-    //TODO Make unmatched elements be vectors of strings.
     let unmatched = { 0: [], 1: []};
-    // Check characters in direction given by lookingForward. 
-    // If a bracket is encountered, extend selection to the corresponding matching bracket.
-    // If the termination of a code line is reached, we are finished
-    // extending in that direction. Continue until there are no more unmatched brackets, 
-    // and we have reached the ends of the code lines both forwards and backwards.
     while (!flagAbort && !(flagsFinish[0] && flagsFinish[1])) {
         let { nextChar, nextPos, isEndOfCodeLine, isEndOfFile } = getNextChar(poss[lookingForward ? 1 : 0], lookingForward, getLineFromCache, getEndsInOperatorFromCache, lineCount);
         poss[lookingForward ? 1 : 0] = nextPos;
